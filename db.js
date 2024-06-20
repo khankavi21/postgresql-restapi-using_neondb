893c996632ae01a23fedf02a0801f88cb36f25fd
kavi khna <khankavi21@gmail.com>
2024-06-20 12:55:26 +0530

    db.js updated and modified and everything is set

--- db.js
+++ db.js
@@ -1,35 +1,48 @@
-import postgres  from 'postgres';
-import {config} from 'dotenv';
+import postgres from 'postgres';
+import { config } from 'dotenv';
 config();
 
-const {PGHOST,PGDATABASE, PGUSER,PGPASSWORD ,ENDPOINT_ID} = process.env;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
 export const sql = postgres({
-    host:PGHOST,
-    database:PGDATABASE,
-    username:PGUSER,
-    password:PGPASSWORD,
-    port:5432,
-    ssl:'require',
-    connection:{
-        options:`project=${ENDPOINT_ID}`
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: 'require',
+    connection: {
+        options: `project=${ENDPOINT_ID}`
     }
 });
 
-const createTableQuery = sql`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL UNIQUE,
-        password VARCHAR(100) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
-`;
 
-export const connectTODb = async()=>{
+
+export const connectTODb = async () => {
     try {
-        await createTableQuery;
-        console.log('Table users created successfully');
+        const res = await sql`
+              SELECT *
+              FROM information_schema.tables
+              WHERE table_schema = 'public'
+              AND table_name = 'users'
+        `;
+
+        if (res.length === 0) {
+            const createTableQuery = sql`
+             CREATE TABLE IF NOT EXISTS users (
+             id SERIAL PRIMARY KEY,
+             name VARCHAR(100) NOT NULL,
+             email VARCHAR(100) NOT NULL UNIQUE,
+             password VARCHAR(100) NOT NULL,
+             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+          `;
+            await createTableQuery;
+            console.log('Database connected successfully and Table users created successfully ');
+        }
+        else {
+            console.log('Database connected successfully');
+        }
     } catch (error) {
         console.error('Error fetching PostgreSQL version:', error)
     }
